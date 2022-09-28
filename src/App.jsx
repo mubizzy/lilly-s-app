9989f76d22ad742e265549a2d86aadd0eb6c5cb3
unknown <ajibolamubarak304@gmail.com>
2022-09-28 11:31:45 +0100

    App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold underline text-center">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
