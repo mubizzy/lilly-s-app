d49f9c922fb3a98be8ccc34672a1eb24a398bbd8
unknown <ajibolamubarak304@gmail.com>
2022-09-30 19:08:10 +0100

    intro

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#00302E",
+        pink: "#E2B887",
+        green: " #00302E",
+        pinky: " #FBDDBB",
+      },
+    },
+  },
+  plugins: [],
+};
