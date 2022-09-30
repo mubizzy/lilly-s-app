b3efcdd4e164474117b52cad9d7c7b7af165cd48
unknown <ajibolamubarak304@gmail.com>
2022-09-30 19:07:03 +0100

    intro

---
+++ src/Section/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+function Navbar() {
+  return (
+    <div className="px-4 py-2 container mx-auto top-0 flex justify-between items-center">
+      <div className=" flex items-center text-white font-bold">
+        <img src={logo} className="mr-4 h-57.48 w- 67 " alt="Neva" /> Lilies
+      </div>
+      <div className="flex gap-8">
+        <div className="fw-bold py-3 text-pinky font-medium">Home</div>
+        <div className="py-3 text-white font-medium">login</div>
+        <button class="bg-pink hover:bg-pink text-green font-semibold py-3 px-6 rounded">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
