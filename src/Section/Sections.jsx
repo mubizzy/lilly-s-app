a8bffdb69deb1953ffd90f51f11315d0d4c0cf9e
unknown <ajibolamubarak304@gmail.com>
2022-10-01 10:45:45 +0100

    login

--- src/Section/Sections.jsx
+++ src/Section/Sections.jsx
@@ -4,7 +4,7 @@ import img2 from "../images/img3.png";
 import img3 from "../images/img4.png";
 const Sections = () => {
   return (
-    <section>
+    <section className="py-16">
       <div className="text-center">
         <h2 className="font-bold text-primary text-4xl py-4">
           Special Meals of the day!
@@ -15,21 +15,43 @@ const Sections = () => {
           and swift delivery to what ever location within Ilorin.
         </p>
       </div>
-      <div className="grid grid-cols-3 px- py-5 mt-4 ">
+      <div className="grid grid-cols-3 px-20 py-20 mt-6 gap-16 ">
         <div className="">
-          <img src={images} alt="noodles" className="w-48 h-48 item-center" />
-          <h3 className="text-center">Stir fry Pasta</h3>
-          <p>Stir fry pasta yada yada yada because of Sesan</p>
+          <div className="lg:pl-20">
+            <img src={images} alt="noodles" className="w-48 h-48  mb-4 " />
+          </div>
+
+          <h3 className="text-center mb-4 text-pink font-bold text-2xl">
+            Stir fry Pasta
+          </h3>
+          <p className="text-center text-white font-normal">
+            Stir fry pasta yada yada yada because of Sesan
+          </p>
         </div>
-        <div>
-          <img src={img2} alt="noodles" className="w-48 h-48" />
-          <h3 className="text-center">Meat Balls</h3>
-          <p>Stir fry pasta yada yada yada because of Sesan</p>
+        <div className="">
+          <div className="lg:pl-20">
+            <img src={img2} alt="noodles" className="w-48 h-48 mb-4" />
+          </div>
+
+          <h3 className="text-center mb-4 text-pink text-2xl font-bold">
+            Meat Balls
+          </h3>
+          <p className="text-center text-white">
+            Stir fry pasta yada yada yada because of Sesan
+          </p>
         </div>
-        <div>
-          <img src={img3} alt="noodles" className="w-48 h-48" />
-          <h3 className="text-center">Burger Meal</h3>
-          <p>Stir fry pasta yada yada yada because of Sesan</p>
+        <div className="">
+          <div className=" lg:pl-20">
+            {" "}
+            <img src={img3} alt="noodles" className="w-48 h-48 mb-4" />
+          </div>
+
+          <h3 className="text-center mb-4 text-pink font-bold text-2xl">
+            Burger Meal
+          </h3>
+          <p className="text-center text-white">
+            Stir fry pasta yada yada yada because of Sesan
+          </p>
         </div>
       </div>
     </section>
