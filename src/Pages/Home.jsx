87a935cde64b0a6223c37265a62296c4fcbd440a
unknown <ajibolamubarak304@gmail.com>
2022-10-01 10:46:01 +0100

    login

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../Section/Hero";
+import Navbar from "../Section/Navbar";
+import Sections from "../Section/Sections";
+
+function Home() {
+  return (
+    <div>
+      <Hero></Hero>
+      <Sections></Sections>
+    </div>
+  );
+}
+
+export default Home;
