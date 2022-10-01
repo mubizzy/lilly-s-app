1b34be1fe0fb752df0109c2fb529a8ad9da9ec0a
unknown <ajibolamubarak304@gmail.com>
2022-10-01 10:46:10 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
 import Navbar from "./Section/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
