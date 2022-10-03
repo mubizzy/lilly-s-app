c55e13cb890cc25232b5f9af6d3d36aac351ff6e
unknown <ajibolamubarak304@gmail.com>
2022-10-03 16:29:46 +0100

    Footer

--- src/Section/Footer.jsx
+++ src/Section/Footer.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="">
+      <div class="footer__data">
+        <h3 class="footer__subtitle">Company</h3>
+        <ul>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              About Us
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="#" class="footer__link">
+              Careers
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer__data">
+        <h3 class="footer__subtitle">Support</h3>
+        <ul>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Help Center
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="#" class="footer__link">
+              Support Center
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer__data">
+        <h3 class="footer__subtitle">Legal</h3>
+        <ul>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Cookies Policy
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="#" class="footer__link">
+              Privacy Policy
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Terms of Service
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Dispute resolution
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
