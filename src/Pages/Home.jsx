232517323b47ff15cc91cb43c9d049c53932a3ab
unknown <ajibolamubarak304@gmail.com>
2022-10-03 16:30:08 +0100

    Footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../Section/Footer";
 import Hero from "../Section/Hero";
 import Sections from "../Section/Sections";
 import Subscribe from "../Section/Subscribe";
@@ -9,6 +10,7 @@ function Home() {
       <Hero></Hero>
       <Sections></Sections>
       <Subscribe></Subscribe>
+      <Footer></Footer>
     </div>
   );
 }
