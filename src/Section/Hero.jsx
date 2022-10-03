f2e2e3cecc5d64533ac33e464bda8fef59a22bde
unknown <ajibolamubarak304@gmail.com>
2022-10-03 17:06:43 +0100

    Hero

--- src/Section/Hero.jsx
+++ src/Section/Hero.jsx
@@ -5,7 +5,7 @@ import logos from "../images/Google Play Badge.svg";
 const Hero = () => {
   return (
     <div className="container px-4 py-20 mx-auto grid md:grid-cols-2 gap-32">
-      <div className="py-16">
+      <div className="py-20  ">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium  text-primary leading-loose">
           Order
           <span className="text-pinky"> food </span>
@@ -17,8 +17,8 @@ const Hero = () => {
           delivered to you in no time. Affordable, tasty and fast!
         </p>
         <div className="mt-4 flex gap-2">
-          <img src={logo} alt="App badge" />
           <img src={logos} alt="Google badge" />
+          <img src={logo} alt="App badge" />
         </div>
       </div>
       <div className="">
