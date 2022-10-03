7cdb7bab76f6553372f3e28b09056f6a3770e912
unknown <ajibolamubarak304@gmail.com>
2022-10-03 17:18:27 +0100

    Footer

--- src/Section/Hero.jsx
+++ src/Section/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="container px-4 py-20 mx-auto grid md:grid-cols-2 gap-32">
       <div className="py-20  ">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium  text-primary leading-loose">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium  text-primary leading-loose ">
           Order
           <span className="text-pinky"> food </span>
           anytime,
@@ -16,7 +16,7 @@ const Hero = () => {
           Browse from our list of specials to place your order and have food
           delivered to you in no time. Affordable, tasty and fast!
         </p>
-        <div className="mt-4 flex gap-2">
+        <div className="mt-8 flex gap-2">
           <img src={logos} alt="Google badge" />
           <img src={logo} alt="App badge" />
         </div>
