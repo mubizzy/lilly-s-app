c14a335605c7bfbd1f7d1f83d86a271effe4d103
unknown <ajibolamubarak304@gmail.com>
2022-10-03 17:11:26 +0100

    Hero

--- src/Section/Footer.jsx
+++ src/Section/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "../images/App Store Badge.svg";
+import logos from "../images/Google Play Badge.svg";
 
 function Footer() {
   return (
@@ -39,6 +41,7 @@ function Footer() {
           </li>
         </ul>
       </div>
+
       <div class="footer__data">
         <h3 class="footer__subtitle">Legal</h3>
         <ul>
@@ -51,6 +54,57 @@ function Footer() {
             <a href="#" class="footer__link">
               Privacy Policy
             </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Terms of Service
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Dispute resolution
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer__data">
+        <h3 class="footer__subtitle">Legal</h3>
+        <ul>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Cookies Policy
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="#" class="footer__link">
+              Privacy Policy
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Terms of Service
+            </a>
+          </li>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              Dispute resolution
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer__data">
+        <h3 class="footer__subtitle">Install App</h3>
+        <ul>
+          <li class="footer__item">
+            <a href="" class="footer__link">
+              <img src={logos} alt="Google badge" />
+            </a>
+          </li>
+          <li class="footer__item">
+            <img src={logo} alt="App badge" />
+            <img src={logo} alt="App badge" />
           </li>
           <li class="footer__item">
             <a href="" class="footer__link">
