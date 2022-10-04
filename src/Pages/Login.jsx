7e36875ff77083623c194c0cd94fcbdb22377fe8
unknown <ajibolamubarak304@gmail.com>
2022-10-04 05:15:20 +0100

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="bg-pinky ">
+      <div className="md:[67%] lg:[50%]">
+        <img src="" alt="" />
+      </div>
+      <div></div>
+    </section>
+  );
 }
 
 export default Login;
