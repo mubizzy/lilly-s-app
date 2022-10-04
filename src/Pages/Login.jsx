8ca60c18137a91f5b7ad7905bf4d0efb38fd1bf1
unknown <ajibolamubarak304@gmail.com>
2022-10-04 07:15:44 +0100

    Log

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import fine from "../images/fine.png";
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -14,20 +15,30 @@ function Login() {
   };
   return (
     <section className=" ">
-      <div className="flex justify-center  item-center px-6 py-12 flex-wrap mx-auto ">
+      <div className="flex justify-center  item-center px-6 py-12 flex-wrap mx-auto  ">
         <div className="md:[67%] lg:[20%] mb-12 md:mb-6">
-          <img src={fine} alt="" className="h-30 w-30" />
+          <img src={fine} alt="" className="" />
         </div>
         <div className="w-full md[67%] lg:w-[40%] ">
           <form>
             <input
               type="text"
-              className="w-full"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300  transition ease-in-out"
               id="email"
               value={email}
               onChange={onChange}
               placeholder="Email address"
             />
+            <div className="relative mb-6">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              />
+            </div>
           </form>
         </div>
       </div>
