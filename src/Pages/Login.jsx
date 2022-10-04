dffd9b54131dab1b0f8a49954cc7959e50bdf483
unknown <ajibolamubarak304@gmail.com>
2022-10-04 06:31:12 +0100

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import fine from "../images/fine.png";
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
   return (
-    <section className="bg-pinky ">
-      <div className="md:[67%] lg:[50%]">
-        <img src="" alt="" />
+    <section className=" ">
+      <div className="flex justify-center  item-center px-6 py-12 flex-wrap mx-auto ">
+        <div className="md:[67%] lg:[20%] mb-12 md:mb-6">
+          <img src={fine} alt="" className="h-100 w-30" />
+        </div>
+        <div className="w-full md[67%] lg:w-[40%] ">
+          <form>
+            <input
+              type="text"
+              className="w-full"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+            />
+          </form>
+        </div>
       </div>
-      <div></div>
     </section>
   );
 }
