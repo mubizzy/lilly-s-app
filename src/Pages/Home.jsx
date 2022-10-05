6bafb312e545d41c3f921f9d6e2b3c7ada952ca9
unknown <ajibolamubarak304@gmail.com>
2022-10-05 12:54:09 +0100

    Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Footer from "../Section/Footer";
 import Hero from "../Section/Hero";
+import Navbar from "../Section/Navbar";
 import Sections from "../Section/Sections";
 import Subscribe from "../Section/Subscribe";
 
 function Home() {
   return (
-    <div>
+    <div className="">
       <Hero></Hero>
       <Sections></Sections>
       <Subscribe></Subscribe>
