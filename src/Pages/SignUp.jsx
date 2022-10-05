fbe2e1b941c6317507a176264c4f4539918bd44c
unknown <ajibolamubarak304@gmail.com>
2022-10-05 13:45:08 +0100

    signUp

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 import fine from "../images/fine.png";
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
   return (
     <section className=" ">
-      <div className="flex justify-center item-center px-6 py-12 flex-wrap container mx-auto w-8/12 ">
+      <div className="flex justify-center item-center px-6 py-12 flex-wrap  container mx-auto w-8/12 ">
         <div className=" w-1/2">
           <img src={fine} alt="image" className="" />
         </div>
         <div
-          className="w-1/2 flex-wrap bg-white 
+          className="w-1/2   flex-wrap bg-white 
         "
         >
           <div className=" py-8 ">
             <h1 className="text-3xl text-center  font-semi-bold py-6  ">
-              welcome to Lillies
+              Welcome to Lilies!
             </h1>
             <form className="px-12 ">
               <input
@@ -61,18 +73,18 @@ function SignUp() {
                 className="w-full bg-green text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-green-800 mb-4"
                 type="submit"
               >
-                LOGIN
+                SIGN UP
               </button>
-              <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg px-2">
-                <p className="text-sm">
+              <div className="text-center ">
+                <p className="text-sm font-bold">
+                  Already have an account.
                   <Link
-                    to="/sign-up"
-                    className="text-green hover:text-blue-800 transition duration-200 ease-in-out "
+                    to="/login"
+                    className="text-green hover:text-blue-800 transition duration-200 ease-in-out font-normal"
                   >
-                    Create an account
+                    LOGIN
                   </Link>
                 </p>
-                <p className="text-sm">Forgot Password</p>
               </div>
             </form>
           </div>
