b10e25e55eff1c7be99001afa2f1a14a9d2349b1
unknown <ajibolamubarak304@gmail.com>
2022-10-17 13:58:41 +0100

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkpk51wLx3kcCF2qZZnYLXDsK0LDK2eKU",
+  authDomain: "lillies-app.firebaseapp.com",
+  projectId: "lillies-app",
+  storageBucket: "lillies-app.appspot.com",
+  messagingSenderId: "950801089264",
+  appId: "1:950801089264:web:0b67c08a7691df3ce7f835",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
