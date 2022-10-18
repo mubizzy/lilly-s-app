12f2c40b1f799f81cc198368ed4897ed05d6a03d
unknown <ajibolamubarak304@gmail.com>
2022-10-18 14:11:25 +0100

    signUp

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -11,6 +11,7 @@ import {
 import { db } from "../firebase";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -50,7 +51,7 @@ function SignUp() {
       await setDoc(doc(db, "users", user.uid), formDataCopy);
       navigate("/");
     } catch (error) {
-      // console.log(error);
+      toast.error("Something went wrong with the registration");
     }
   }
   return (
