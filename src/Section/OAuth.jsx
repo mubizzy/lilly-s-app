c818a26a2b7d888f1e95d24076e516a2a829a299
unknown <ajibolamubarak304@gmail.com>
2022-10-19 14:28:52 +0100

    OAuth

--- src/Section/OAuth.jsx
+++ src/Section/OAuth.jsx
@@ -10,6 +10,19 @@ const OAuth = () => {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
       console.log(user);
+
+      // check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
     } catch (error) {
       toast.error("Could not authorize with Google");
       console.log(error);
